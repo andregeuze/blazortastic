24a25255624d82513f3fc36861872e99b4765a5c
Victor de Baare <victor.de.baare@gmail.com>
2022-09-09 10:11:42 +0200

    Update to add a generator

---
+++ Generator/RazorBlazorGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace Generator
+{
+    [Generator]
+    public class RazorBlazorGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("Test.g.cs", "namespace Test { public class Test { } }");
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+    }
+}
